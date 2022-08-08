197d5799138c432750929e017b3fc5b7d6dd474d
Adiks <apacholak97@interia.pl>
2022-08-08 11:13:38 +0200

    beer service route configuration

--- src/main/java/com/adrian/mssc/brewery/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/adrian/mssc/brewery/gateway/config/LocalHostRouteConfig.java
@@ -4,18 +4,15 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Profile("google")
 @Configuration
-public class GoogleConfig {
+public class LocalHostRouteConfig {
 
     @Bean
-    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/googlesearch")
-                        .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
-                        .uri("https://google.com"))
+                .route("beer-service", r -> r.path("/api/v1/beer", "/api/v1/beer/*")
+                        .uri("http://localhost:8080"))
                 .build();
     }
 }
